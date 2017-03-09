cb736b40138feaf940a34b4cc25388b8a3f0ae14
Kayode Oguntimehin <kayode.oguntimehin@konga.com>
2017-03-09 12:46:20 +0100

    Initial Commit

---
+++ GroupGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroupGenerator
+//
+//  Created by Kayode Oguntimehin on 09/03/2017.
+//  Copyright © 2017 Kayode Oguntimehin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
